7abd775a7ff624d484040b7be6de91bc81ea20b3
Zane Tan <zxnetxn@Zanes-MacBook-Pro.local>
2021-04-28 16:18:18 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20011280/demomysecondapp/LoginPagewrksht2.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20011280.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPagewrksht2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_pagewrksht2);
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id20011280/demomysecondapp/MainActivity2D.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20011280.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2D extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_d);
+    }
+}
